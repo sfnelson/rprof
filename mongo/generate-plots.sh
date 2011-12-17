a372d8310568203c76be168de7b47cd276daf0b8
Stephen Nelson <stephen@sfnelson.org>
2011-12-18 12:47:08 +1300

    parameterised plot script and class table support

--- mongo/generate-plots.sh
+++ mongo/generate-plots.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-mongo --quiet alex-proj.pvt:27017 plot-fine.js > fieldsFine.dat
-mongo --quiet alex-proj.pvt:27017 plot-coarse.js > fieldsCoarse.dat
-mongo --quiet alex-proj.pvt:27017 plot-constructor.js > constructorReturn.dat
+
+#mongo --quiet alex-proj.pvt:27017 plot-fine.js > fieldsFine.dat
+#mongo --quiet alex-proj.pvt:27017 plot-coarse.js > fieldsCoarse.dat
+#mongo --quiet alex-proj.pvt:27017 plot-constructor.js > constructorReturn.dat
+
+function print_table {
+	cat plot.js | sed -e "s/TABLE_NAME/$1/" | sed -e "s/RESULT_SET/$2/" | mongo --quiet alex-proj.pvt:27017 | grep -v "^bye$" > $3
+}
+
+for TYPE in resultsClasses resultsObjects; do
+	for SET in fieldsFine fieldsCoarse constructorReturn firstEquals composite; do
+		print_table $TYPE $SET "$TYPE-$SET.dat"
+	done
+done
--- mongo/plot.js
+++ mongo/plot.js
@@ -1,19 +1,17 @@
-var dbs = db.getMongo().getDBNames();
+var dbs = db.getMongo().getDBNames().sort();
 print("Dataset\tImmutable/No Equals\tImmutable/Equals\tMutable/No Equals\tMutable/Equals\tTotal");
 for (i in dbs) {
     var d = db.getMongo().getDB(dbs[i]);
     var cols = d.getCollectionNames();
     for (j in cols) {
-        if (cols[j] == "results") {
+        if (cols[j] == "TABLE_NAME") {
             var output = d.properties.findOne().benchmark;
-            //var output = "" + count;
-            d.results.find({ _id : "fieldsFine" }).forEach(function (r) {
+            d.results.find({ _id : "RESULT_SET" }).forEach(function (r) {
                 output += "\t" + r.value.immutable.noequals;
                 output += "\t" + r.value.immutable.equals;
                 output += "\t" + r.value.mutable.noequals;
                 output += "\t" + r.value.mutable.equals;
-                output += "\t" + (r.value.immutable.noequals + r.value.immutable.equals
-                    + r.value.mutable.noequals + r.value.mutable.equals);
+		output += "\t" + r.value.total;
             });
             print(output);
         }

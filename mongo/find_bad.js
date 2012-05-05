8330959b4268ba083553a6f47f9b0a59fb97b199
Stephen Nelson <stephen@sfnelson.org>
2012-05-06 04:47:35 +1200

    added find bad data script

---
+++ mongo/find_bad.js
@@ -0,0 +1,8 @@
+function findBad() {
+	var bad = db.field.results.find({declaredFinal:true,final:false},{_id:1,name:1});
+	bad.forEach(function (f) {
+		printjson(f.name);
+		var cls = db.fields.findOne({_id:f._id}).owner;
+		printjson(db.instances.findOne({type:cls,fields:{$elemMatch:{_id:f._id,writes:{$gt:1}}}}));
+	});
+}
